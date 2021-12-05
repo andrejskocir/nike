c8d73344ccddf89a5fba5b51fede5a1e611ab93f
andrejskocir <andrej.skocir@gmail.com>
2021-12-05 21:00:17 +0100

    commit on 'master'

--- js/app.js
+++ js/app.js
@@ -1,18 +1,45 @@
 //Movement Animation to happen
 const card = document.querySelector(".card");
-const container = document.querySelector(".container");
 const card1 = document.querySelector(".card topright");
+const container = document.querySelector(".container");
+const container1 = document.querySelector(".container topright");
+
 //Items
-const sneaker = document.querySelector(".sneaker img");
+const use = document.querySelector(".sneaker");
+const use1 = document.querySelector(".sneaker");
 //Animate In
-container.addEventListener("mouseenter", (e) => {
-  card.style.transition = "none";
+card.addEventListener("mouseenter", (e) => { 
   //Popout
-  sneaker.style.transform = "rotateZ(-45deg)";
+  use.style.transform = "rotateZ(-45deg)";
 });
+
+
 //Animate Out
-container.addEventListener("mouseleave", (e) => {
-  card.style.transition = "all 0.5s ease";
+card.addEventListener("mouseleave", (e) => {
+card.style.transition = "all 0.5s ease";
   //Popback
-  sneaker.style.transform = "rotateZ(0deg)";
+  use.style.transform = "rotateZ(0deg)";
 });
+
+window.onload = function() {
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+
+  context.beginPath();
+  context.moveTo(15, 4);
+  context.bezierCurveTo(15, 4, 11.181, 14.419, 13, 17);
+  context.bezierCurveTo(14.819, 19.581, 17.525, 23.677, 26, 21);
+  context.bezierCurveTo(34.475, 18.323, 96, 1, 96, 1);
+  context.bezierCurveTo(96, 1, 23, 35, 23, 35);
+  context.bezierCurveTo(23, 35, 11.004, 38.839, 5, 34);
+  context.bezierCurveTo(-1.004, 29.161, 2.649, 18.931, 5, 15);
+  context.bezierCurveTo(7.351, 11.069, 15, 4, 15, 4);
+  context.bezierCurveTo(15, 4, 15, 4, 15, 4);
+  context.lineWidth = 1;
+
+  context.strokeStyle = '#fff';
+  context.stroke();
+
+  context.fillStyle = '#fff';
+  context.fill();
+}
