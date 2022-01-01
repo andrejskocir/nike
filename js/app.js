de5cce1b2f1b796fd1a9e4c0edca79928b0a7533
andrejskocir <andrej.skocir@gmail.com>
2022-01-01 18:45:03 +0100

    comit on 'master'

--- js/app.js
+++ js/app.js
@@ -4,38 +4,57 @@ const card1 = document.getElementById("cardTR");
 const card2 = document.getElementById("cardBL");
 const card3 = document.getElementById("cardBR");
 const krog = document.getElementById("krog");
+const area1 = document.getElementById("area1");
 
 //Items
 const use = document.querySelector(".sneaker");
 const use1 = document.getElementById("sneakerTR");
 const use2 = document.getElementById("sneakerBL");
 const use3 = document.getElementById("sneakerBR");
-const sneakerPrvi = document.getElementById("krog");
-const naslov = document.getElementById("naslov");
+const sneakerPrvi = document.getElementById("sneakerPrvi");
+const tekst = document.getElementById("tekst");
+const vip = document.getElementById("vip");
 
 //Animate In
-naslov.addEventListener("mouseenter", (e) => { 
+tekst.addEventListener("mouseenter", (e) => { 
   //Popout
-  naslov.style.transform = "translateY(-10%)";
+  tekst.style.transform = "translateY(-10%)";
 });
 //Animate Out
-naslov.addEventListener("mouseleave", (e) => {
-naslov.style.transition = "all 0.75s ease";
+tekst.addEventListener("mouseleave", (e) => {
+  tekst.style.transition = "all 0.75s ease";
   //Popback
-  naslov.style.transform = "translateY(0%)";
+  tekst.style.transform = "translateY(0%)";
 });
 
 
 //Animate In
 krog.addEventListener("mouseenter", (e) => { 
   //Popout
-  sneakerPrvi.style.transform = "rotateZ(20deg) scale(1.1)";
+  krog.style.transform = "rotateZ(20deg) scale(1.05)";
+  sneakerPrvi.style.transform = "scale(1.08)";
 });
 //Animate Out
 krog.addEventListener("mouseleave", (e) => {
 krog.style.transition = "all 0.75s ease";
   //Popback
-  sneakerPrvi.style.transform = "rotateZ(0deg)";
+  sneakerPrvi.style.transform = "scale(1)";
+  krog.style.transform = "rotateZ(0deg) scale(1)";
+});
+
+//Animate In
+area1.addEventListener("mouseenter", (e) => { 
+  //Popout
+
+  vip.style.transform = "perspective(1000px) translateZ(80px)";
+  
+});
+//Animate Out
+area1.addEventListener("mouseleave", (e) => {
+vip.style.transition = "all 0.75s ease";
+
+  //Popback
+  vip.style.transform = "translateZ(0px)";
 });
 
 
@@ -110,3 +129,52 @@ window.onload = function() {
   context.fillStyle = '#fff';
   context.fill();
 }
+
+//Curved text
+const ZadNaslov = document.getElementById("ZadNaslov");
+ZadNaslov.innerHTML = ZadNaslov.innerText.split("").map(
+  (char,i) =>
+  `<span style="transform: rotate(${i * 10-67.5}deg)">${char}</span>`
+
+).join("");
+
+
+
+/*
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(75px)";
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+});*/
