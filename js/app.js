6d3e1d740966a8ded42f6c6856df4bb5a7b6d029
andrejskocir <andrej.skocir@gmail.com>
2022-01-20 15:38:36 +0100

    commit on 'master'

--- js/app.js
+++ js/app.js
@@ -198,3 +198,37 @@ pur4.onclick = function(){
     }
   }
 }
+
+
+//Get the button
+var mybutton = document.getElementById("scrollToTop");
+var ikona = document.getElementById("ikona");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+mybutton.addEventListener("mouseenter",(e)=>{
+  mybutton.style.transition = "0.5s ease";
+  ikona.style.transition = "0.5s ease";
+  mybutton.style.background= "#545454";
+  ikona.style.color = "#fff";
+})
+mybutton.addEventListener("mouseleave",(e)=>{
+  mybutton.style.background= "transparent";
+  ikona.style.color= "#545454";
+
+})
